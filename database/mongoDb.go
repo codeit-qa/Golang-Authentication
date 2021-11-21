d805f1cec6586d4edf1f250885c6f1063b201326
Muhammad Qazi <tigerbhai888@gmail.com>
2021-11-21 12:52:00 +0200

    jwt authentication token for (signin,signup)

--- database/mongoDb.go
+++ database/mongoDb.go
@@ -7,9 +7,11 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+
+	models "GO/models"
 )
 
-func HandleDatabaseInsert(DBname string, CollectionName string, user interface{}) bool {
+func HandleDatabaseInsert(DBname string, CollectionName string, email string, phone int, password string, fname string, lname string, uid string, created time.Time, updated time.Time) bool {
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://127.0.0.1:27017"))
 	if err != nil {
@@ -18,7 +20,16 @@ func HandleDatabaseInsert(DBname string, CollectionName string, user interface{}
 
 	collection := client.Database(DBname).Collection(CollectionName)
 
-	_, errInsert := collection.InsertOne(ctx, user)
+	_, errInsert := collection.InsertOne(ctx, bson.M{
+		"email":      email,
+		"phone":      phone,
+		"password":   password,
+		"first_name": fname,
+		"last_name":  lname,
+		"user_id":    uid,
+		"created_at": created,
+		"updated_at": updated,
+	})
 
 	if errInsert != nil {
 		return false
@@ -27,7 +38,9 @@ func HandleDatabaseInsert(DBname string, CollectionName string, user interface{}
 	return true
 }
 
-func HandleAuthentication(email string, password string, DBname string, CollectionName string, user interface{}) bool {
+func HandleAuthentication(email string, password string, DBname string, CollectionName string) (bool, string, string, string, string) {
+
+	var user models.AuthenticationModel
 
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://127.0.0.1:27017"))
@@ -40,9 +53,9 @@ func HandleAuthentication(email string, password string, DBname string, Collecti
 	errFind := collection.FindOne(ctx, bson.M{"email": email, "password": password}).Decode(&user)
 
 	if errFind != nil {
-		return false
+		return false, "", "", "", ""
 
 	}
 
-	return true
+	return true, user.Email, user.First_name, user.Last_name, user.User_id
 }
